b9b46a2aae891a96e894e6c5a6492f15117d0957
Yogadimas <yogadimaspambudi@gmail.com>
2023-11-29 17:48:42 +0700

    Timeout di Test

---
+++ src/test/kotlin/com/yogadimas/_18_timeout_di_test/SlowTest.kt
@@ -0,0 +1,28 @@
+package com.yogadimas._18_timeout_di_test
+
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Timeout
+import java.util.concurrent.TimeUnit
+
+
+@DisplayName("_18_timeout_di_test - SlowTest")
+class SlowTest {
+
+    /**
+     * Timeout di Test
+     * ● Kadang kita ingin memastikan bahwa sebuah unit test berjalan tidak lebih dari sekian detik
+     * ● Misal ketika kasus kita ingin memastikan kode program kita mempunyai performa bagus dan cepat
+     * ● JUnit memiliki fitur timeout, yaitu memastikan bahwa unit test berjalan tidak lebih dari waktu yang
+     * ditentukan, jika melebihi waktu yang ditentukan, secara otomatis unit test tersebut akan gagal
+     * ● Kita bisa menggunakan annotation @Timeout untuk melakukan hal tersebut
+     */
+    @Test
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)
+    fun testSlow1(){
+        // Thread.sleep(10000) // 10 detik // gagal, karena unit test hanya mau menunggu 5 detik
+        Thread.sleep(3000) // 3 detik
+    }
+
+
+}
